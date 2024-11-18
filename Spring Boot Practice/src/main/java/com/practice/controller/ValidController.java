fa57102c11463c860dffbff49f302cb74ce94203
IDO <susususu07@naver.com>
2024-11-18 21:36:08 +0900

    Add files via upload

--- Spring Boot Practice/src/main/java/com/practice/controller/ValidController.java
+++ Spring Boot Practice/src/main/java/com/practice/controller/ValidController.java
@@ -1,55 +1,60 @@
-package com.practice.controller;
-
-import org.springframework.ui.Model;
-import com.practice.dto.ValidForm;
-import com.practice.dto.ValidSuperForm;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/valid")
-public class ValidController {
-
-    @GetMapping("/error")
-    public void get_error() {}
-
-    @GetMapping("/main")
-    public void get_main(Model model) {
-        ValidSuperForm validSuperForm = new ValidSuperForm();
-        ValidForm validForm = new ValidForm();
-        validSuperForm.setValidForm(validForm);
-        model.addAttribute("superForm", validSuperForm);
-    }
-
-    @PostMapping("/main")
-    public String post_main(
-//            @Validated ValidForm validForm,
-            @Validated ValidSuperForm validSuperForm,
-            BindingResult bindingResult, // 필수 구성요소, 없으면 바로 오류터짐
-            Model model
-
-    ) {
-        model.addAttribute("superForm", validSuperForm);
-        if(bindingResult.hasErrors()) {
-            System.out.println("에러가 있어요.");
-
-//            bindingResult.getAllErrors().forEach(
-//                    error -> System.out.println(error)
-//            );
-
-            bindingResult.getAllErrors().forEach(error -> {
-                System.out.println(error.getDefaultMessage());
-            });
-            return "redirect:/valid/error";
-        }
-//        System.out.println(validForm);
-        System.out.println(validSuperForm);
-        return "redirect:/valid/main";
-    }
-
-
-}
+//package com.practice.controller;
+//
+//import org.springframework.ui.Model;
+//import com.practice.dto.ValidForm;
+//import com.practice.dto.ValidSuperForm;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.validation.FieldError;
+//import org.springframework.validation.ObjectError;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/valid")
+//public class ValidController {
+//
+//    @GetMapping("/error")
+//    public void get_error() {}
+//
+//    @GetMapping("/main")
+//    public void get_main(Model model) {
+//        ValidSuperForm validSuperForm = new ValidSuperForm();
+//        ValidForm validForm = new ValidForm();
+//        validSuperForm.setValidForm(validForm);
+//        model.addAttribute("superForm", validSuperForm);
+//    }
+//
+//    @PostMapping("/main")
+//    public String post_main(
+////            @Validated ValidForm validForm,
+//            @Validated ValidSuperForm validSuperForm,
+//            BindingResult bindingResult, // 필수 구성요소, 없으면 바로 오류터짐
+//            Model model
+//
+//    ) {
+//
+//        // 1. 모델을 사용하는 방법
+//        model.addAttribute("superForm", validSuperForm);
+//        if (bindingResult.hasErrors()) {
+//            System.out.println("에러가 있어요.");
+//
+////            bindingResult.getAllErrors().forEach(
+////                    error -> System.out.println(error)
+////            );
+//
+//            bindingResult.getAllErrors().forEach(error -> {
+//                System.out.println(error.getDefaultMessage());
+//            });
+//            return "redirect:/valid/error";
+//        }
+////        System.out.println(validForm);
+//        System.out.println(validSuperForm);
+//        return "redirect:/valid/main";
+//
+//    }
+//
+//}
--- Spring Boot Practice/src/main/java/com/practice/dto/ValidSuperForm.java
+++ Spring Boot Practice/src/main/java/com/practice/dto/ValidSuperForm.java
@@ -1,17 +1,30 @@
 package com.practice.dto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 @Setter
 @ToString
 public class ValidSuperForm {
-    @NotBlank
+    @NotBlank(message = "(2) 슈퍼폼) (아이디) id는 비울 수 없습니다")
     private String id;
-    @Valid
+    @NotNull(message = "(2) 슈퍼폼) (나이) {0}은 비울 수 없습니다")
+//    @Min(value = 0, message = "0이상의 값을 사용하세요")
+    @Range(min = 0, max = 200, message = "(2) 슈퍼폼) (나이) {0}는 {2} ~ {1} 값만 넣으세요")
+    private Integer age;
+
+    @NotBlank
+    private String addr;
+
+    private String phone;
+    private String email;
+    //    @Valid
     private ValidForm validForm;
 }
